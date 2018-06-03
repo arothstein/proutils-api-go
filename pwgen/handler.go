a1b303678ce8d837eeeea113b0e4744c55e98639
Aaron Rothstein <aaron.rothstein@wunita.com>
2018-06-03 02:19:04 -0500

    Used JSON Decode.

--- pwgen/handler.go
+++ pwgen/handler.go
@@ -7,31 +7,39 @@ package pwgen
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+// PwRequest is JSON request format received via POST
+type PwRequest struct {
+	WordCount  int  `json:"WordCount"`
+	AddInt     bool `json:"AddInt"`
+	AddSpecial bool `json:"AddSpecial"`
+	PwCount    int  `json:"PwCount"`
+}
 
 // Passwords is the JSON response record for the password-generator POST request.
 type passwordList struct {
 	Passwords []string `json:"Passwords"`
 }
 
-// HandlePwGen returns a list of generated passwords based on input criteria..
+// HandlePwGen returns a list of generated passwords based on input criteria.
 func HandlePwGen(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	// Unmarshal
+	var req PwRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 
 	// variable to hold the generated passwords
 	pwSet := make([]string, 1)
-	wordCount, err := strconv.Atoi(params.ByName("wordCount"))
-	handleError(err)
-	addInt, err := strconv.ParseBool(params.ByName("addInt"))
-	handleError(err)
-	addSpecial, err := strconv.ParseBool(params.ByName("addSpecial"))
-	handleError(err)
-	pwCount, err := strconv.Atoi(params.ByName("pwCount"))
 
-	for i := 0; i < pwCount; i++ {
-		pw, err := NewPassword(wordCount, addInt, addSpecial)
+	for i := 0; i < req.PwCount; i++ {
+		pw, err := NewPassword(req.WordCount, req.AddInt, req.AddSpecial)
 		handleError(err)
 		pwSet = append(pwSet, pw)
 	}
